cfc9530881bb7d04d6d3591a4c5181c8b2185eb0
Yahya <yahya.qandel@gmail.com>
2016-10-16 06:10:55 +0200

    site class done

--- sites/Site.py
+++ sites/Site.py
@@ -1,25 +1,41 @@
-from pymongo import MongoClient
-import urllib2
+import ConfigParser
+from dbconnection.DBConnection import DBConnection
+import httplib
+import mechanize
+
 
 class Site(object):
-    def __init__(self,site_info):
+    def __init__(self,site_info,dbname="default"):
         self.site_info = site_info
-        self.save()
+        self._db=dbname
+        if not 'priority' in self.site_info:
+            site_priority = {'priority': 1}
+            self.site_info.update(site_priority)
 
     def save(self):
         site_dict = self.site_info
-        if not 'priority' in site_dict:
-            site_priority = {'priority':1}
-            site_dict.update(site_priority)
+        collection = self.setup_dbClient()
+        sites_query = collection.find(
+            {"title":site_dict['title']
+            }
+        )
+        if sites_query.count()==0:
+            collection.insert_one(site_dict)
 
-        print site_dict['url']
-            # print site_dict
+    def setup_dbClient(self):
+        config = ConfigParser.ConfigParser()
+        config.read('Configuration.cfg')
+        if self._db=="default":
+            self._db = config.get('DataDB', 'DBNAME')
+        client = DBConnection().client
+        sites_collection = config.get('DataDB', 'SITES_COLLECTION')
+        return client[self._db][sites_collection]
+
+    def check_code_status(self):
         try:
-            urllib2.urlopen(site_dict['url'])
-            print site_dict['url']
-            print conn
-            conn.request("HEAD", "/")
-            r1 = conn.getresponse()
-            print r1
+            httplib.HTTPConnection.debuglevel = 1
+            request = mechanize.Request(self.site_info['url'], headers={'User-Agent': 'Python-mechanize'})
+            response = mechanize.urlopen(request)
+            return response.code
         except Exception as exc:
             raise Exception('Invalid site url')
--- tests/test_Site.py
+++ tests/test_Site.py
@@ -1,27 +1,50 @@
 import unittest
-
+import ConfigParser
 from dbconnection.DBConnection import DBConnection
 from sites.Site import Site
 
 class TestSite(unittest.TestCase):
 
   def setUp(self):
-      self.site_info_without_priority = {'url':'www.yahyaqandel.com','name':'YahyaQandel','title':'Yahya Adel Qandel'}
-      self.site_info_invalid_url = {'url':'http://zsadf.com','name':'YahyaQandel','title':'Yahya Adel Qandel','priority':'5'}
+      self.site_info_without_priority = {'url':'yahyaqandel.com','name':'YahyaQandel','title':'Yahya Adel Qandel'}
+      self.site_info_invalid_url = {'url':'http://zadgxe.com','name':'YahyaQandel','title':'Yahya Adel Qandel','priority':'5'}
       self.site_info = {'url': 'http://yahyaqandel.com', 'name': 'YahyaQandel', 'title': 'Yahya Adel Qandel',
                     'priority': '5'}
+      self.test_db = None
 
   def test_site_object_not_null(self):
       site_obj = Site(self.site_info)
       self.assertIsNotNone(site_obj)
 
-  def test_site_save_prioity_ignored(self):
+  def test_site_prioity_ignored(self):
       site_obj = Site(self.site_info_without_priority)
-      site_obj.save()
       self.assertEqual(site_obj.site_info['priority'],1)
 
 
-  # def test_site_save_invalid_url(self):
-  #     site_obj = Site(self.site_info_invalid_url)
-  #     self.assertRaises(Exception, site_obj.save())
+  def test_site_invalid_url(self):
+      site_obj = Site(self.site_info_invalid_url)
+      self.assertRaises(Exception, site_obj.check_code_status)
 
+
+  def test_site_valid_url(self):
+      site_obj = Site(self.site_info)
+      self.assertEqual(200, site_obj.check_code_status())
+
+  def test_site_save(self):
+      config = ConfigParser.ConfigParser()
+      config.read('Configuration.cfg')
+      self.test_db = config.get('DataDB', 'DBNAME_TEST')
+      site_test_obj = Site(self.site_info,self.test_db)
+      site_test_obj.save()
+      client = DBConnection().client
+      sites_collection = config.get('DataDB', 'SITES_COLLECTION')
+      collection = client[self.test_db][sites_collection]
+      sites_query = collection.find(
+          {"title": self.site_info['title']}
+      )
+      self.assertEqual(sites_query.count(),1)
+
+  def tearDown(self):
+      if self.test_db:
+          client = DBConnection().client
+          client.drop_database(self.test_db)
